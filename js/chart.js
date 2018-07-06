5779f3f4e0f664f0cb90fff09784874c7f44a84e
Austin Flanagan <apflanagan@gmail.com>
2018-07-06 13:31:03 -0500

    1.20.2

--- js/chart.js
+++ js/chart.js
@@ -3,7 +3,7 @@
 // });
 // google.charts.setOnLoadCallback(drawChart);
 var bluetoothDevice = null;
-var versionNumber = 1.20;
+var versionNumber = 1.20.2;
 var microbitUUID = 'e95d0000-251d-470a-a062-fa1922dfa9a8';
 var accServiceUUID = 'e95d0753-251d-470a-a062-fa1922dfa9a8';
 var accDataUUID = 'e95dca4b-251d-470a-a062-fa1922dfa9a8'
@@ -160,7 +160,7 @@ function handleValueChange(event) {
 
 function onLogButton() {
   data_container.innerHTML = "";
-  for (var i = 0; i < accData.length(); i++) {
+  for (var i = 0; i < accData.length; i++) {
     for (var j = 0; j < 3; j++) {
       data_container.write('<p>' + accData[i][j] + '</p>');
     }
