93a81580374ec083cd610f8368d615894a01631c
Austin Flanagan <apflanagan@gmail.com>
2018-07-06 12:13:25 -0500

    1.8

--- js/chart.js
+++ js/chart.js
@@ -3,7 +3,7 @@
 // });
 // google.charts.setOnLoadCallback(drawChart);
 var bluetoothDevice = null;
-var versionNumber = 1.7;
+var versionNumber = 1.8;
 var microbitUUID = 'e95d0000-251d-470a-a062-fa1922dfa9a8';
 var accServiceUUID = 'e95d0753-251d-470a-a062-fa1922dfa9a8';
 var accDataUUID = 'e95dca4b-251d-470a-a062-fa1922dfa9a8'
@@ -11,7 +11,7 @@ var accPeriod = 'e95dfb24-251d-470a-a062-fa1922dfa9a8'
 var AccelerometerData = null;
 var AccelerometerPeriod = null;
 var AccelerometerService = null;
-var accData = new Int16Array(3);
+var accData = new Int16Array();
 var gattServer;
 
 function onConnectClick() {
@@ -113,6 +113,24 @@ function onPeriodButtonClick() {
     });
 }
 
+function setPeriodTo1() {
+  return (AccelerometerService ? Promise.resolve() : onConnectClick())
+  .then(_ => {
+    console.log('Found Period Characteristic');
+    return AccelerometerService.getCharacteristic(accPeriod);
+  })
+  .then(characteristic => {
+    AccelerometerPeriod = characteristic;
+    return AccelerometerPeriod.writeValue(1);
+  })
+  .then(value => {
+    console.log('New Accelerometer period is: ' + value.getUint8(0));
+  })
+  .catch(error => {
+    console.log('Argh! ' + error);
+  });
+}
+
 function onDisconnectButton() {
   if (!bluetoothDevice) {
     console.log('No Device Found');
@@ -131,18 +149,22 @@ function onDisconnected(event) {
   console.log('> Bluetooth Device disconnected');
   document.getElementById('disconnectButton').innerHTML = "Disconnected";
   document.getElementById('connectButton').innerHTML = "Connect";
+  document.getElementById('startButton').innerHTML = "Read";
 
 }
 
 function handleValueChange(event) {
-  AcceleratorX = event.target.value.getUint16(0) / 1000.0;
+  AcceleratorX = event.target.value.getInt16(0) / 1000.0;
   console.log('x: ' + AcceleratorX);
 
-  AcceleratorY = event.target.value.getUint16(2) / 1000.0;
+  AcceleratorY = event.target.value.getInt16(2) / 1000.0;
   console.log('y: ' + AcceleratorY);
 
-  AcceleratorZ = event.target.value.getUint16(4) / 1000.0;
+  AcceleratorZ = event.target.value.getInt16(4) / 1000.0;
   console.log('z: ' + AcceleratorZ);
+  var accItem = new Int16Array();
+  accItem = [AcceleratorX, AcceleratorY, AcceleratorZ];
+  accData.push(accItem);
 }
 
 // function drawChart() {
