8eb510ce69afade2c45e7916ac28dcc2dd446ea6
Austin Flanagan <apflanagan@gmail.com>
2018-07-06 17:13:43 -0500

    1.22.22

--- js/chart.js
+++ js/chart.js
@@ -3,7 +3,7 @@ google.charts.load('current', {
 });
 
 var bluetoothDevice = null;
-var versionNumber = '1.22.21';
+var versionNumber = '1.22.22';
 var microbitUUID = 'e95d0000-251d-470a-a062-fa1922dfa9a8';
 var accServiceUUID = 'e95d0753-251d-470a-a062-fa1922dfa9a8';
 var accDataUUID = 'e95dca4b-251d-470a-a062-fa1922dfa9a8'
@@ -49,9 +49,9 @@ function onConnectClick() {
 function onButtonClick() {
   if (reading) {
     AccelerometerData.stopNotifications();
-    reading = false;
     clearInterval(graphUpdate);
     document.getElementById('startButton').innerHTML = "Read";
+    reading = false;
   } else {
     reading = true;
     var graphUpdate = setInterval(function() {
