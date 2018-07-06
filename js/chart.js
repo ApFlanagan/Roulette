ffa736c3e841be30b1985933e8d0585f4b409a51
Austin Flanagan <apflanagan@gmail.com>
2018-07-06 15:13:28 -0500

    1.22.2

--- js/chart.js
+++ js/chart.js
@@ -3,7 +3,7 @@ google.charts.load('current', {
 });
 google.charts.setOnLoadCallback(drawChart);
 var bluetoothDevice = null;
-var versionNumber = '1.22.1';
+var versionNumber = '1.22.2';
 var microbitUUID = 'e95d0000-251d-470a-a062-fa1922dfa9a8';
 var accServiceUUID = 'e95d0753-251d-470a-a062-fa1922dfa9a8';
 var accDataUUID = 'e95dca4b-251d-470a-a062-fa1922dfa9a8'
