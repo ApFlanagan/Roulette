b04dcd1adebbab08eb6065a97ea0e5baad9b9abd
Austin Flanagan <apflanagan@gmail.com>
2018-07-06 15:00:42 -0500

    1.21.7

--- js/chart.js
+++ js/chart.js
@@ -3,7 +3,7 @@
 // });
 // google.charts.setOnLoadCallback(drawChart);
 var bluetoothDevice = null;
-var versionNumber = '1.21.6';
+var versionNumber = '1.21.7';
 var microbitUUID = 'e95d0000-251d-470a-a062-fa1922dfa9a8';
 var accServiceUUID = 'e95d0753-251d-470a-a062-fa1922dfa9a8';
 var accDataUUID = 'e95dca4b-251d-470a-a062-fa1922dfa9a8'
@@ -137,10 +137,7 @@ function onDisconnected(event) {
 }
 
 function handleValueChange(event) {
-  var rawTime = new Date;
-  rawTime = Date.now();
-  var timeStamp = rawTime.getHours() + ':' + rawTime.getMinutes() + ':' + rawTime.getSeconds() + ':' + rawTime.getMilliseconds();
-
+  var rtimeStamp = Date.now();
   console.log(timeStamp);
 
   AcceleratorX = event.target.value.getInt16(0, 1);
