d7465472549982da0e53a6eed302ecd126dae127
Austin Flanagan <Apflanagan95@gmail.com>
2018-12-09 16:46:37 -0600

    console log

--- js/chart.js
+++ js/chart.js
@@ -3,7 +3,7 @@ google.charts.load('current', {
 });
 
 var bluetoothDevice = null;
-var versionNumber = '1.24.01';
+var versionNumber = '1.24.02';
 var microbitUUID = 'e95d0000-251d-470a-a062-fa1922dfa9a8';
 var accServiceUUID = 'e95d0753-251d-470a-a062-fa1922dfa9a8';
 var accDataUUID = 'e95dca4b-251d-470a-a062-fa1922dfa9a8'
@@ -249,13 +249,14 @@ function isInArray(value, array) {
 
 function onLogButton() {
   var logString = JSON.stringify(logData);
-  let newWindow = window.open("about:blank", "", "_blank");
-
-  let textBlock = logData
-
-  if (newWindow) {
-    newWindow.document.write(textBlock);
-  }
+  console.log(logData);
+  // let newWindow = window.open("about:blank", "", "_blank");
+  //
+  // let textBlock = logData
+  //
+  // if (newWindow) {
+  //   newWindow.document.write(textBlock);
+  // }
   // chrome.downloads.showDefaultFolder()
   // chrome.downloads.download({
   //   url: "data:text/plain," + myString,
